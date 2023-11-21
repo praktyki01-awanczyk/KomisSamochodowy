cb7d7eb99805525f92ed1d1ad22d60d2a47539eb
uczen-admin <uczen-admin@DESKTOP-OU55F78>
2023-11-21 08:30:34 +0100

    Dodaj pliki projektów.

---
+++ KomisSamochodowy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KomisSamochodowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
